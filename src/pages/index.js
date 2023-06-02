63a43869dfa8ea7984f5641854a61485d0b641fa
Tyler Tellez <softwaretjt@gmail.com>
2023-06-01 17:13:53 -0700

    Sprint 8. 1st commit for review

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Section from "../components/Section.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
+import "./index.css";
 
 const initialCards = [
   {
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // connect plugin
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  devtool: "inline-source-map",
+  entry: {
+    main: "./src/pages/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "",
+  },
+  target: ["web", "es5"],
+  stats: "errors-only",
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+    open: true,
+    liveReload: true,
+    hot: false,
+  },
+  module: {
+    rules: [
+      // this is an array of rules
+      // add an object containing rules for Babel to it
+      {
+        // a regular expression that searches for all js files
+        test: /\.js$/,
+        // all files must be processed by babel-loader
+        loader: "babel-loader",
+        // exclude the node_modules folder, we don't need to process files in it
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+
+            options: { importLoaders: 1 },
+          },
+          "postcss-loader",
+        ],
+      },
+      {
+        // add the rule for processing files
+        test: /\.(png|svg|jpg|jpeg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // path to our index.html file
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
