c355c1acd07d0240b60a71a674d2f2dc005d60b8
Tyler Tellez <softwaretjt@gmail.com>
2023-06-02 20:36:30 -0700

    Sprint 8. 4th commit after reviewer comments

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,18 +1,18 @@
 export default class UserInfo {
   constructor({ userNameSelector, userJobSelector }) {
     this._userNameElement = document.querySelector(userNameSelector);
-    this._userJobSelector = document.querySelector(userJobSelector);
+    this._userJobElement = document.querySelector(userJobSelector);
   }
 
   getUserInfo() {
     return {
       userName: this._userNameElement.textContent,
-      userJobDescription: this._userJobSelector.textContent,
+      userJobDescription: this._userJobElement.textContent,
     };
   }
 
   setUserInfo({ title, job }) {
     this._userNameElement.textContent = title;
-    this._userJobSelector.textContent = job;
+    this._userJobElement.textContent = job;
   }
 }
