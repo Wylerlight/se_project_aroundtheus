f73ab34bf73f3c81aaae6b859ec1b9f0753ee25a
Tyler Tellez <softwaretjt@gmail.com>
2023-08-02 12:38:11 -0700

    message

--- src/components/PopupCardDeleteVerify.js
+++ src/components/PopupCardDeleteVerify.js
@@ -1,21 +1,21 @@
-import Popup from "./Popup.js";
+import Popup from './Popup.js';
 
 export default class PopupCardDeleteVerify extends Popup {
   constructor(popupSelector, handleFormSubmit) {
     super(popupSelector);
 
     this._verifyDeleteCardButton = this._popupElement.querySelector(
-      "#modal-verify-delete-card-button"
+      '#modal-verify-delete-card-button'
     );
     this._handleFormSubmit = handleFormSubmit;
     this._handleSubmit = this._handleSubmit.bind(this);
 
-    this._popupForm = this._popupElement.querySelector(".modal__form");
-    this._submitButton = this._popupForm.querySelector(".modal__button");
+    this._popupForm = this._popupElement.querySelector('.modal__form');
+    this._submitButton = this._popupForm.querySelector('.modal__button');
     this._submitButtonText = this._submitButton.textContent;
   }
 
-  renderLoading(isLoading, loadingText = "Saving...") {
+  renderLoading(isLoading, loadingText = 'Saving...') {
     if (isLoading) {
       this._submitButton.textContent = loadingText;
     } else {
@@ -30,7 +30,7 @@ export default class PopupCardDeleteVerify extends Popup {
   setEventListeners() {
     super.setEventListeners();
 
-    this._verifyDeleteCardButton.addEventListener("click", this._handleSubmit);
+    this._verifyDeleteCardButton.addEventListener('click', this._handleSubmit);
   }
 
   _handleSubmit() {
